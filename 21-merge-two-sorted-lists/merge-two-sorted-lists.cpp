091029ba5fc61a9ee16ee9d65a88678e5f959389
Anmol Tiwari <oanmoltiwari7@gmail.com>
2024-06-02 23:29:04 +0530

    Time: 2 ms (83.55%) | Memory: 18.3 MB (40.61%) - LeetSync

--- 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/merge-two-sorted-lists.cpp
@@ -10,33 +10,30 @@
  */
 class Solution {
 public:
-    ListNode* convertarr2ll(vector<int> &arr){
-        ListNode* head=new ListNode(arr[0]);
-        ListNode* mover=head;
-        for(int i=1;i<arr.size();i++){
-            ListNode* temp=new ListNode(arr[i]);
-            mover->next=temp;
-            mover=temp;
-        }
-        return head;
-    }
     ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
-        if(list1==NULL && list2==NULL){
-            return nullptr;
+        ListNode* dummy=new ListNode(-1);
+        ListNode* t1=list1;
+        ListNode* t2=list2;
+        ListNode* temp=dummy;
+        while(t1!=NULL && t2!=NULL){
+            if(t1->val<t2->val){
+                temp->next=t1;
+                temp=t1;
+                t1=t1->next;
+            }
+            else{
+                temp->next=t2;
+                temp=t2;
+                t2=t2->next;
+            }
         }
-        vector<int> arr;
-        ListNode* temp1=list1;
-        ListNode* temp2=list2;
-        while(temp1!=NULL){
-            arr.push_back(temp1->val);
-            temp1=temp1->next;
+        if(t1){
+            temp->next=t1;
         }
-        while(temp2!=NULL){
-            arr.push_back(temp2->val);
-            temp2=temp2->next;
+        else{
+            temp->next=t2;
         }
-        sort(arr.begin(),arr.end());
-        ListNode* head=convertarr2ll(arr);
-        return head;
+        return dummy->next;
+
     }
 };
